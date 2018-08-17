865ca0966ed35f97aa0626c4312fd8b09fe85670
tmcknew <tmcknew@users.noreply.github.com>
2018-08-16 21:41:33 -0700

    input vaildation for state

--- best.R
+++ best.R
@@ -1,6 +1,12 @@
 best <- function(state, outcome) {
     ## Read outcome data
+    outcome <- read.csv("data/outcome-of-care-measures.csv", colClasses = "character")
     ## Check that state and outcome are valid
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
+    if (!(state %in% outcome[,7])){
+        # error, state invalid
+        stop("invalid state")
+    }
+    
+    
+    ## Return hospital name in that state with lowest 30-day death rate
 }

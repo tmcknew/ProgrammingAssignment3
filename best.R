0f21795d74c8ee4c47591159eb0e2f4230fa0ded
tmcknew <tmcknew@users.noreply.github.com>
2018-08-17 20:01:31 -0700

    adds functional outcome selection testing, column lookup in data table

--- best.R
+++ best.R
@@ -8,11 +8,20 @@ best <- function(state, outcome_selected) {
     }
     # heart attack”, “heart failure”, or “pneumonia”
     outcome_options <- c("heart attack", "heart failure", "pneumonia")
+    # make vector of possible selections
     table_cols <- c(11,17,23)
+    # make data.frame lookup of selections matched to corresponding 
+    # table cols in data
     lookup <- data.frame(outcome_options, table_cols)
-    column <- lookup[ match(outcome_selected, outcome_options, nomatch=0), table_cols ]
-    #column <- table_cols[ which( input == lookup$options ) ]
-    #selected <- match(outcome, options, nomatch=0)
-    
+    # get column number from lookup
+    found <- match(outcome_selected, outcome_options, nomatch = 0)
+    column <- lookup[ found , "table_cols" ]
+    # if we didn't fiund a column, exit
+    if (length(column) == 0) {
+        stop("invalid outcome")
+     }
+    print(column)
+    print(lookup[lookup$table_cols == column,])
     ## Return hospital name in that state with lowest 30-day death rate
 }
+

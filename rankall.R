f5f09be40df6d2710a4f2b0ae9f7edf0fd471934
tmcknew <tmcknew@users.noreply.github.com>
2018-08-20 21:57:41 -0700

    df to list by state

--- rankall.R
+++ rankall.R
@@ -1,4 +1,4 @@
-rankhospital <- function( state, outcome_selected, num = "best" ) {
+rankall <- function( outcome_selected, num = "best" ) {
     
     ## Read outcome data from file, keep strings for now
     outcome_data <- read.csv( "data/outcome-of-care-measures.csv", 
@@ -7,13 +7,11 @@ rankhospital <- function( state, outcome_selected, num = "best" ) {
     outcome_data$State <- as.factor( outcome_data$State )
     
     ## Check that state is valid
-    if ( !(state %in% levels( outcome_data$State ) ) ){
-        # error, state invalid. halt.
-        stop( "invalid state" )
-    }
-    # subset on selected state
-    state_data <- subset( outcome_data, outcome_data$State == state )
-    
+    # if ( !(state %in% levels( outcome_data$State ) ) ){
+    #     # error, state invalid. halt.
+    #     stop( "invalid state" )
+    # }
+
     # create dataframe "lookup" for selection and column reference
     outcome_options <- c( "heart attack", "heart failure", "pneumonia" )
     table_cols <- c( 11, 17, 23 )
@@ -30,23 +28,32 @@ rankhospital <- function( state, outcome_selected, num = "best" ) {
     outcome_column <- lookup[ found , "table_cols" ]
     outcome_name <- colnames( outcome_data )[ outcome_column ]
     
+    # subset on selected state
+    # state_data <- subset( outcome_data, outcome_data$State == state )
+    
     # convert selected outcome column to numeric for sort
-    state_data[ , outcome_name ] <- as.numeric( state_data[ , outcome_name ] )
+    outcome_data[ , outcome_name ] <- as.numeric( outcome_data[ , outcome_name ] )
 
-    # sort on outcome, name. store ordering
-    ordering <- order(state_data[ , outcome_name ], 
-                      state_data[,"Hospital.Name"], na.last = NA)
-
-    ## return hospital name in that state with specified rank
+    # sort on State, outcome, name. store ordering
+    ordering <- order(outcome_data[,"State"], 
+                      outcome_data[ , outcome_name ], 
+                      outcome_data[,"Hospital.Name"], na.last = NA)
+    
+    # drop unused columns
+    outcome_data <- outcome_data[ ordering, c("State", "Hospital.Name", outcome_name)]
+    
+    # split on State
+    state_data <- split(outcome_data, "State")
+    
+    ## convert num to ranking
     # state_data[ ordering, ][[1,"Hospital.Name"]]
     if (num == "best"){ num <- 1}
-    if (num == "worst"){ num <- length(ordering)}
-    if( num > length(ordering)){
-        return_value = NA;
-    }
-    else{
-        return_value <- state_data[ ordering, ][num, "Hospital.Name"]
-    }
-    return_value
+    # if (num == "worst"){ num <- length(ordering)}
+
+    # if( num > length(ordering)){
+    #     return_value = NA;
+    # }
+ 
+    
 }
 
